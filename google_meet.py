94feba2f8c388ca469c65ef7413c7d476a8739db
Shandon Mith <Shandonmith@gmail.com>
2022-07-17 18:19:44 -0400

    chromedriver bug

--- google_meet.py
+++ google_meet.py
@@ -1,15 +1,33 @@
 from lib2to3.pgen2 import driver
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.chrome.options import Options
 import datetime
 import time
 import os
 import keyboard
 
+
+#Loads chrome with default settings
+opt=Options()
+opt.add_argument("start-maximized")
+opt.add_argument("--disable-extensions")
+opt.add_argument('--ignore-ssl-errors=yes')
+opt.add_argument('--ignore-certificate-errors')
+# Pass the argument 1 to allow and 2 to block
+opt.add_experimental_option("prefs", { \
+"profile.default_content_setting_values.media_stream_mic": 1,
+"profile.default_content_setting_values.media_stream_camera": 1,
+"profile.default_content_setting_values.geolocation": 1,
+"profile.default_content_setting_values.notifications": 1
+})
+
+#Gives path to chrome webdriver and loads classroom webpage
+
+
 class meet_bot:
 	def __init__(self):
-		self.bot = webdriver.Chrome("chromedriver.exe")
+		self.bot = webdriver.Chrome(chrome_options=opt, executable_path='C:\Program Files (x86)\chromedriver.exe')
 	def login(self,email,pas):
 		bot = self.bot
 		bot.get("https://accounts.google.com/ServiceLogin/signinchooser?ltmpl=meet&continue=https%3A%2F%2Fmeet.google.com%3Fhs%3D193&&o_ref=https%3A%2F%2Fwww.google.com%2F&_ga=2.238615491.291103277.1658079668-1292948687.1658079668&flowName=GlifWebSignIn&flowEntry=ServiceLogin")
